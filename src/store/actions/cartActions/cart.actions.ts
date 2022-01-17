15f7a8145872d54cb03b80ace630847b535b135a
Thiago Pereira <thiago_salper@hotmail.com>
2022-01-17 00:22:18 -0300

    refactor: path cart reducer

--- src/store/actions/cartActions/cart.actions.ts
+++ src/store/actions/cartActions/cart.actions.ts
@@ -1,8 +1,46 @@
 import { useDispatch } from "react-redux"
-import { CART_ACTIONS } from ".";
-import { CartItem, Product } from "../../data";
-import { useSelectorTyped } from "../hooks/useSelectorTyped";
-import { APP_ACTIONS } from "./appActions";
+import { CartItem, Product } from "../../../data";
+import { useSelectorTyped } from "../../hooks/useSelectorTyped";
+import { APP_ACTIONS } from "../appActions";
+
+export enum CART_ACTIONS {
+  ADD_PRODUCT = 'ADD_PRODUCT',
+  REMOVE_PRODUCT = 'REMOVE_PRODUCT',
+  CLEAR_CART = 'CLEAR_CART',
+  INCREMENT_CART = 'INCREMENT_CART',
+  DECREMENT_CART = 'DECREMENT_CART',
+}
+
+interface actionAddProduct {
+  type: CART_ACTIONS.ADD_PRODUCT;
+  payload: CartItem[];
+}
+
+interface actionRemoveProduct {
+  type: CART_ACTIONS.REMOVE_PRODUCT;
+  payload: CartItem[];
+}
+
+interface actionClearCart {
+  type: CART_ACTIONS.CLEAR_CART;
+}
+
+interface actionIncrementCart {
+  type: CART_ACTIONS.INCREMENT_CART;
+  payload: CartItem[];
+}
+
+interface actionDecrementCart {
+  type: CART_ACTIONS.DECREMENT_CART;
+  payload: CartItem[];
+}
+
+export type ActionCartReducer = 
+  actionAddProduct | 
+  actionRemoveProduct | 
+  actionClearCart |
+  actionIncrementCart | 
+  actionDecrementCart;
 
 interface CartActionsInterface {
   add(item: Product): void;
--- src/store/actions/index.ts
+++ src/store/actions/index.ts
@@ -1,42 +1,10 @@
-import { CartItem } from "../../data";
+export { 
+  default as useCartActions, 
+  CART_ACTIONS, 
+  ActionCartReducer
+} from './cartActions/cart.actions';
 
-export enum CART_ACTIONS {
-  ADD_PRODUCT = 'ADD_PRODUCT',
-  REMOVE_PRODUCT = 'REMOVE_PRODUCT',
-  CLEAR_CART = 'CLEAR_CART',
-  INCREMENT_CART = 'INCREMENT_CART',
-  DECREMENT_CART = 'DECREMENT_CART',
-}
-
-interface actionAddProduct {
-  type: CART_ACTIONS.ADD_PRODUCT;
-  payload: CartItem[];
-}
-
-interface actionRemoveProduct {
-  type: CART_ACTIONS.REMOVE_PRODUCT;
-  payload: CartItem[];
-}
-
-interface actionClearCart {
-  type: CART_ACTIONS.CLEAR_CART;
-}
-
-interface actionIncrementCart {
-  type: CART_ACTIONS.INCREMENT_CART;
-  payload: CartItem[];
-}
-
-interface actionDecrementCart {
-  type: CART_ACTIONS.DECREMENT_CART;
-  payload: CartItem[];
-}
-
-export type ActionCartReducer = 
-  actionAddProduct | 
-  actionRemoveProduct | 
-  actionClearCart |
-  actionIncrementCart | 
-  actionDecrementCart;
-
-export { default as useCartActions } from './cart.actions';
+export { 
+  default as useAppActions,
+  APP_ACTIONS
+} from './appActions';
